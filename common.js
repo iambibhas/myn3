fa29b63cf099aeddd10dfa0f8e9a633069145b60
Bibhas C Debnath <iambibhas@gmail.com>
2012-09-23 23:38:34 +0530

    defined few models.

--- common.js
+++ common.js
@@ -1,9 +1,11 @@
+var mongoose = require('mongoose');
 var options = {
     // required modules
     // util: require('util'),
     // fs: require('fs'),
     // ======================
     // global configurations
+    db: mongoose.createConnection('mongodb://localhost/myn3db'),
     host: '127.0.0.1',
     port: 1337,
     template_path: "./templates/",
---
+++ models.js
@@ -0,0 +1,34 @@
+var common = require('./common');
+var Schema = mongoose.Schema;
+var ObjectId = Schema.ObjectId;
+
+/*
+Note:
+1. Users can register username.
+(I'd be damn pissed if I have to use different nicks everytime in a room, 
+just because someone else is using it.)
+2. Password and Email verification is needed only when registering.
+*/
+var User = new Schema({
+    nick: {
+        type:String,
+        required: true,
+        unique: true
+    },
+    email: String,
+    passwd: String,
+    verified: Boolean
+});
+
+var Room = new Schema({
+    name: {
+        type:String,
+        required: true
+    },
+    slug: {
+        type:String,
+        required: true,
+        unique: true
+    },
+    tagline: String,
+});
